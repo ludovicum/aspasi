a9eeeede4634527702ebafd1a34034b416e6c017
Ludovicum <luiscamargocandelas@gmail.com>
2022-12-27 13:14:28 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hola esta es mi primera prueba con React</h1>
+      <h2>Estoy empezando a entender como va esto</h2>
     </div>
   );
 }
